621d6ebe39113ecbb45c02e9be76f33fc1789204
lzt666 <1592959724@qq.com>
2019-06-15 09:24:38 +0800

    练习

---
+++ 上课练习代码/zuoye4（1).cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	
+int i,j,k,n;
+printf("水仙花数为：\n");
+
+for(n=0;n<1000;n++)
+{
+	i=n/100;
+	j=(n-i*100)/10;
+	k=n%10;
+	if(i*i*i+j*j*j+k*k*k==n)
+	{
+		printf("%d",n);
+	}
+  }
+} 
+
