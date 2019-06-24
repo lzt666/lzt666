e0d7e76408cc7013db9f80cab344ccd48fac4e0a
lzt666 <1592959724@qq.com>
2019-06-24 23:45:29 +0800

    练习代码

---
+++ 上课练习代码/函数数组数据交换.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void InitArry(int arr[],int size){
+	for(int i =0;i<=size;i++){
+		arr[i]=i;
+		
+	}
+}
+void EmptArry(int arr[],int size){
+	memset(arr,0,sizeof(arr[0])*size);
+} 
+void ReverseArry(int arr[],int size)
+{
+	int left=0;
+	int right=size-1;
+	while(left<right)
+	{
+		int tmp=arr[left];
+		arr[left]=arr[right];
+		arr[right]=tmp;
+		left++;
+		right--;
+		
+		
+	}
+}
+void PrintArry(int arr[],int size){
+	for(int i=0;i<size;i++){
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+	}
+int main(){
+		int a[20];
+		int _size=sizeof(a)/sizeof(a[0]) ;
+		InitArry(a,_size);
+			PrintArry(a,_size);
+	//	EmptArry(a,_size);
+		ReverseArry(a,_size);
+		PrintArry(a,_size);
+		system("pause");
+		return 0; 
+	}
