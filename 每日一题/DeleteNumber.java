d336ae2c45b8ee6afd93ddb4c70aa0188566ef1c
lzt666 <1592959724@qq.com>
2019-07-14 08:52:16 +0800

    删除数字

---
+++ 每日一题/DeleteNumber.java
@@ -0,0 +1,27 @@
+package javaclass;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DeleteNumber {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        while (scanner.hasNext()){
+            int n=scanner.nextInt();
+            if (n>1000){
+                n=999;
+            }
+            List<Integer>list=new ArrayList<>();
+            for (int i=0;i<n;i++){
+                list.add(i);
+            }
+            int i=0;
+            while (list.size()>1){
+                i=(i+2)%list.size();
+                list.remove(i);
+            }
+            System.out.println(list.get(0));
+        }
+    }
+}
